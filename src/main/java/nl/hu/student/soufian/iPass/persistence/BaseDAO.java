cfd43a0da3185a799864720cd3b8733d1ecb9133
soufian <soufian.tichattibin@student.hu.nl>
2017-06-15 13:14:35 +0200

    NIEUWE BASEDAO
    op hoop van de zege

--- src/main/java/nl/hu/student/soufian/iPass/persistence/BaseDAO.java
+++ src/main/java/nl/hu/student/soufian/iPass/persistence/BaseDAO.java
@@ -7,33 +7,34 @@ import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class BaseDAO {
-	private DataSource connectionPool;
+
+	private static DataSource connectionPool = null;
 
 	public BaseDAO() {
-		try {
-			final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
-			if (DATABASE_URL_PROP != null) {
-				URI dbUri = new URI(DATABASE_URL_PROP);
-				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-				BasicDataSource pool = new BasicDataSource();
-				if (dbUri.getUserInfo() != null) {
-					pool.setUsername(dbUri.getUserInfo().split(":")[0]);
-					pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+		if (connectionPool == null) {
+			try {
+				final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+				if (DATABASE_URL_PROP != null) {
+					URI dbUri = new URI(DATABASE_URL_PROP);
+					String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+					BasicDataSource pool = new BasicDataSource();
+					if (dbUri.getUserInfo() != null) {
+						pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+						pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+					}
+					pool.setDriverClassName("org.postgresql.Driver");
+					pool.setUrl(dbUrl);
+					pool.setInitialSize(1);
+					connectionPool = pool;
+				} else {
+					InitialContext ic = new InitialContext();
+					connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
 				}
-				pool.setDriverClassName("org.postgresql.Driver");
-				pool.setUrl(dbUrl);
-				pool.setInitialSize(1);
-
-				connectionPool = pool;
-			} else {
-				InitialContext ic = new InitialContext();
-				connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
 		}
 	}
-
 	protected final Connection getConnection() {
 		try {
 			return connectionPool.getConnection();
